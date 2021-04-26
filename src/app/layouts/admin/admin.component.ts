37e6fc5954a058eb0829a81571246f61ebf98189
mohamedaliznidi <mohamedali.znidi1@esprit.tn>
2021-04-26 21:34:18 +0200

    dashboard

--- src/app/layouts/admin/admin.component.ts
+++ src/app/layouts/admin/admin.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  styleUrls: ['./admin.component.css'],
 })
 export class AdminComponent implements OnInit {
+  sideBarOpen = true;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit() {}
+
+  sideBarToggler() {
+    this.sideBarOpen = !this.sideBarOpen;
   }
-
 }
--- src/app/layouts/admin/admin.module.ts
+++ src/app/layouts/admin/admin.module.ts
@@ -5,9 +5,27 @@ import { RouterModule } from '@angular/router';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { DashboardComponent } from 'src/app/components/dashboard/dashboard.component';
 import { HttpClientModule } from '@angular/common/http';
-
+import { DashboardService } from 'src/app/components/dashboard/dashboard.service';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatCardModule } from '@angular/material/card';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
+import { FlexLayoutModule } from '@angular/flex-layout';
 @NgModule({
   declarations: [AdminComponent, DashboardComponent],
-  imports: [CommonModule, RouterModule, SharedModule, HttpClientModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    SharedModule,
+    HttpClientModule,
+    MatSidenavModule,
+    MatDividerModule,
+    FlexLayoutModule,
+    MatCardModule,
+    MatPaginatorModule,
+    MatTableModule,
+  ],
+  providers: [DashboardService],
 })
 export class AdminModule {}
